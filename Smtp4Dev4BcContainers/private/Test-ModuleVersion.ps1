f825ad2c0b221c0fe2f71219d324dbfc4c984936
Blank, Christoph <christoph.blank@kumavision.com>
2020-06-05 19:05:06 +0200

    fix error in version check

--- Smtp4Dev4BcContainers/private/Test-ModuleVersion.ps1
+++ Smtp4Dev4BcContainers/private/Test-ModuleVersion.ps1
@@ -2,8 +2,12 @@
     process {
         $name = $MyInvocation.MyCommand.Module
         Write-Verbose "Current Module Name is '$name'"
-        $installedVersion = (get-installedmodule -Name Smtp4Dev4BcContainers | Sort-Object -Property Version | Select-Object -Last 1).Version #$MyInvocation.MyCommand.Module.Version does not work while module loading
+        $installedVersion = (get-installedmodule -Name Smtp4Dev4BcContainers -ErrorAction "SilentContinoue" | Sort-Object -Property Version | Select-Object -Last 1).Version #$MyInvocation.MyCommand.Module.Version does not work while module loading
         Write-Verbose "Current Module Version is '$installedVersion'"
+        if (-not $installedVersion) {
+            #Module is not installed
+            return $true
+        }
         $Response = Invoke-WebRequest -Uri "https://www.powershellgallery.com/api/v2/Packages?`$filter=Id eq '$name' and IsLatestVersion"
         $OnlineVersion = ([xml]($Response.Content)).feed.entry.properties.Version
         Write-Verbose "Online Module Version is '$OnlineVersion'"
